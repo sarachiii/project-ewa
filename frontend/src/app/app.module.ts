a33c6f0c53024d8b364d400a20313739b27a96b6
H.D. Mohammad <hashim.mohammad@hva.nl>
2021-10-11 21:35:29 +0200

    Minor changes to nav bar and more components
    Centered the name on the nav bar
    Added a home component and an error component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/mainpage/nav-bar/nav-bar.component';
+import { HomeComponent } from './components/mainpage/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
