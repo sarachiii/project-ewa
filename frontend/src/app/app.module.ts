a0e394f0593d0b9333d29b86acd4b679c57a5cc1
Jechillo Huang <huangj2@ad.hva.nl>
2021-10-24 12:48:38 +0200

    sensor component testfase

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/mainpage/nav-bar/nav-bar.component';
 import { HomeComponent } from './components/mainpage/home/home.component';
 import { ErrorComponent } from './components/error/error.component';
-import { HeaderComponent } from './components/mainpage/header/header.component';
 import { SensorComponent } from './components/mainpage/sensor/sensor.component';
 
 @NgModule({
@@ -15,7 +14,6 @@ import { SensorComponent } from './components/mainpage/sensor/sensor.component';
     NavBarComponent,
     HomeComponent,
     ErrorComponent,
-    HeaderComponent,
     SensorComponent
   ],
   imports: [
