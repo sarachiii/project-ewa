2ea0d7b88a006d8786de4712f7ca4f569cc3bf93
H.D. Mohammad <hashim.mohammad@hva.nl>
2021-10-29 22:31:30 +0200

    Changed navigation and sensors pages
    - Minor changes to nav so it complies to the redesign
    - Rewritten sensors page
    - Added sensor model for sensor data and sensor widget

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule} from './app-routing.module';
+import { FormsModule } from "@angular/forms";
+import { ChartsModule } from 'ng2-charts';
+
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/mainpage/nav-bar/nav-bar.component';
 import { HomeComponent } from './components/mainpage/home/home.component';
@@ -14,8 +17,6 @@ import { ShareNotesComponent } from './components/share-notes/share-notes.compon
 import { SensorComponent } from './components/mainpage/sensor/sensor.component';
 import { LoginComponent } from './components/mainpage/login/login.component';
 import { RegisterComponent } from './components/mainpage/register/register.component';
-import {FormsModule} from "@angular/forms";
-import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
