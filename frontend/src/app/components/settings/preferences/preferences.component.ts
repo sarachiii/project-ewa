fac8dddacd508d3dae73bbe4bb3390f7de6e653f
H.D. Mohammad <hashim.mohammad@hva.nl>
2021-11-11 20:52:53 +0100

    Added preferences form

--- frontend/src/app/components/settings/preferences/preferences.component.ts
+++ frontend/src/app/components/settings/preferences/preferences.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Role, User } from "../../../models/user";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import {LanguageCode, Preferences} from "../../../models/preferences";
 
 @Component({
   selector: 'app-preferences',
@@ -6,10 +9,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./preferences.component.css']
 })
 export class PreferencesComponent implements OnInit {
+  user: User;
+  copyPrefs: Preferences = <Preferences>{};
+  prefsForm: FormGroup;
+  languages: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.user = new User(0, Role.ADMIN, "Botanist", "Sjors", "Peters",
+      "https://yt3.ggpht.com/fAfB4LQvATPHhF9ou35zv5FZmXVhMtGnW_vZQNpyd_Krkzasu48k53I3UTIxcqNyMioqK4PR0w=s900-c-k-c0x00ffffff-no-rj",
+      "sjors.peters@climatecleanup.org", "password1");
+    this.copyPrefs = Object.assign<Preferences, Preferences>(this.copyPrefs, this.user.preferences);
+    this.languages = [{ 'code': 'en_GB', 'lang': 'English'}, { 'code': 'nl_NL', 'lang': 'Dutch'}]
+    this.prefsFormInit();
   }
 
+  ngOnInit(): void {
+    this.onReset();
+  }
+
+  get language() {
+    return this.prefsForm.get('language');
+  }
+
+  get colorblindness() {
+    return this.prefsForm.get('colorblindness');
+  }
+
+  get darkMode() {
+    return this.prefsForm.get('darkMode');
+  }
+
+  private prefsFormInit() {
+    this.prefsForm = new FormGroup({
+      language: new FormControl('', Validators.required),
+      colorblindness: new FormControl(''),
+      darkMode: new FormControl('')
+    })
+  }
+
+  onSubmit(): void {
+    console.log(this.prefsForm.value)
+    if (this.prefsForm.valid) {
+      // TODO: Talk to service to talk with the backend
+    } else {
+      // fail
+    }
+  }
+
+  onReset(): void {
+    this.prefsForm.reset();
+    this.prefsFormInit()
+    this.prefsForm.patchValue(this.copyPrefs);
+  }
 }
--- frontend/src/app/models/preferences.ts
+++ frontend/src/app/models/preferences.ts
@@ -1,4 +1,4 @@
-enum LanguageCode {
+export enum LanguageCode {
   en_GB = "en_GB",
   nl_NL = "nl_NL"
 }
@@ -8,11 +8,16 @@ export class Preferences {
   language: LanguageCode;
   colorblindness: boolean;
   darkMode: boolean;
+  private static readonly _langCodes = Object.values(LanguageCode);
 
   constructor(userId?: number, language?: LanguageCode, colorblindness?: boolean, darkMode?: boolean) {
     this.userId = userId || 0;
     this.language = language || LanguageCode.en_GB;
     this.colorblindness = colorblindness || false;
     this.darkMode = darkMode || false;
+  }
+
+  static get langCodes() {
+    return Preferences._langCodes;
   }
 }
