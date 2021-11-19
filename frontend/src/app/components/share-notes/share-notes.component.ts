689778131f4dcb588543aef107bf878f0faee326
Nazlıcan Eren <nazlican.eren@hva.nl>
2021-11-19 23:10:28 +0100

    Create note now uses user data

--- frontend/src/app/components/share-notes/share-notes.component.ts
+++ frontend/src/app/components/share-notes/share-notes.component.ts
@@ -1,9 +1,11 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
-import {HttpClient} from "@angular/common/http";
 import {NotesService} from "../../services/notes.service";
 import {Note} from "../../models/note";
-import {DatePipe} from "@angular/common";
+import {WebStorageService} from "../../services/storage/web-storage.service";
+import {UserService} from "../../services/user.service";
+import {User} from "../../models/user";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-share-notes',
@@ -13,18 +15,27 @@ import {DatePipe} from "@angular/common";
 export class ShareNotesComponent implements OnInit {
 
   @Output() unselectedEvent = new EventEmitter();
-  title: string | undefined;
-  text: string | undefined;
+  title: string;
+  text: string;
   note: Note;
+  user: User | null;
+  private userSubscription: Subscription;
 
 
-  constructor(private notesService: NotesService, private router: Router, private activatedRoute: ActivatedRoute) {
+  constructor(private notesService: NotesService, private router: Router, private activatedRoute: ActivatedRoute,
+              private webStorageService: WebStorageService, private userService: UserService) {
     setInterval(() => {
       this.currentDate()
     }, 1000)
+
   }
 
+
   ngOnInit(): void {
+    this.userSubscription = this.userService.loggedUser$.subscribe(value => {
+      this.user = value;
+      console.log(this.user);
+    })
   }
 
   onReturnToNotes(title: string, text: string) {
@@ -36,23 +47,14 @@ export class ShareNotesComponent implements OnInit {
     var date = new Date();
     date.setHours(date.getHours() + 1);
     var isodate = date.toISOString().replace(/\..+/, '');
-    // userId & workfield are still hard coded.
-    // noteId gets an id assigned in the database.
-    this.notesService.addNote(new Note(0, 1, "B", isodate, title, text, "Sjors Peters"))
+    this.notesService.addNote(new Note(0, this.user.id, this.user.specialty.charAt(0),
+      isodate, title, text, this.user.firstName))
     this.unselectedEvent.emit(true);
   }
 
-  onDisable() {
-    return true;
-  }
-
   currentDate() {
-    let dateFormat = new Date().toLocaleString('en-GB', {
-      day: 'numeric',
-      month: 'long',
-      hour: 'numeric',
-      minute: 'numeric'
-    });
-    return dateFormat;
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    return new Date().getDay() + " " + months[new Date().getMonth()] + " " +
+      new Date().getFullYear() + ", " + new Date().getHours() + ":" + new Date().getMinutes();
   }
 }
