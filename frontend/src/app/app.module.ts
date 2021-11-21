7b7dc277edab4802076a057660929ac8163446d6
Jechillo Huang <huangj2@ad.hva.nl>
2021-11-21 21:17:26 +0100

    Issue 9 sensor last commit
    The last version of sensors page for sprint 2

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -18,7 +18,6 @@ import { ShareNotesComponent } from './components/share-notes/share-notes.compon
 import { SensorComponent } from './components/mainpage/sensor/sensor.component';
 import { LoginComponent } from './components/mainpage/login/login.component';
 import { RegisterComponent } from './components/mainpage/register/register.component';
-import {HttpClientModule} from "@angular/common/http";
 import {HttpClient} from "@angular/common/http";
 import {SimpleNotificationsModule} from "angular2-notifications";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
--- frontend/src/app/components/mainpage/sensor/sensor.component.ts
+++ frontend/src/app/components/mainpage/sensor/sensor.component.ts
@@ -2,67 +2,77 @@ import {Component, OnInit} from '@angular/core';
 import {Factor, Sensor, ValueType} from "../../../models/sensor";
 import {HttpClient} from "@angular/common/http";
 import {NotificationsService} from "angular2-notifications";
+import {SensorsService} from "../../../services/sensors.service";
 
 @Component({
   selector: 'app-sensor',
   templateUrl: './sensor.component.html',
-  styleUrls: ['./sensor.component.css']
+  styleUrls: ['./sensor.component.css'],
+  providers: [SensorsService]
 })
 export class SensorComponent implements OnInit {
 
-  alert:boolean=false
+  sensorsData: any;
+  alert: boolean = false
   sensors: Array<Sensor>;
 
-  constructor(private http: HttpClient, private service: NotificationsService) {
+  constructor(private http: HttpClient, private service: NotificationsService, private sensorsService: SensorsService) {
     this.sensors = Sensor.generateSensors();
+    this.sensorsData = new Map();
+    for (const sensor of this.sensors) {
+      this.sensorsData.set(sensor.nameCamelCase, sensor)
+
+    }
+
   }
 
- onSuccess(message) {
+  onSuccess(message) {
     this.service.success("Success", message, {
       position: ['bottom', 'left'],
       timeOut: 4000,
       animate: 'fade',
       showProgressBar: true
     });
- }
+    let postData = {
+      ghId: 200,
+      userId: 1,
+      ...this.sensors.reduce((sensorsData, sensor) => {
+        sensorsData[sensor.nameCamelCase] = sensor.value;
 
-  ngOnInit(): void {
-    this.onCreatePost("test");
-    this.getPosts();
+        return sensorsData
+      }, {})
+    }
+    console.log(postData)
+    this.sensorsService.postSensorData(postData)
+      .subscribe(responseData =>
+        console.log(responseData))
   }
 
-  onCreatePost(postData: string){
-    //This method sends a Http request
-    console.log(postData)
-    this.http.post(
-      "http://www.sneltec.com/hva/hva.php" ,
-      postData)
-      .subscribe(responseData =>
-      console.log(responseData))
-  };
+  ngOnInit(): void {
+    this.sensorsService.getSensorData(200).subscribe(response => {
+      console.log(response)
+      console.log(JSON.parse(response['msg']))
+    })
+    console.log(this.sensors)
+
+  }
+
 
   decrementValue(sensor: Sensor): void {
-    if (!isNaN(<number>sensor.value) && !isNaN(parseFloat(<string>sensor.value))){
+    if (!isNaN(<number>sensor.value) && !isNaN(parseFloat(<string>sensor.value))) {
       sensor.value = parseFloat(<string>sensor.value);
       if (sensor.value >= sensor.numberRange[0] + 1 && sensor.value <= sensor.numberRange[1]) sensor.value--;
     }
   }
 
   incrementValue(sensor: Sensor): void {
-    if (!isNaN(<number>sensor.value) && !isNaN(parseFloat(<string>sensor.value))){
+    if (!isNaN(<number>sensor.value) && !isNaN(parseFloat(<string>sensor.value))) {
       sensor.value = parseFloat(<string>sensor.value);
       if (sensor.value >= sensor.numberRange[0] && sensor.value <= sensor.numberRange[1] - 1) sensor.value++;
     }
-    this.alert=true
+    this.alert = true
     // this.sensors.reset({})
   }
 
-
-
-  private getPosts() {
-    this.http.get("http://www.sneltec.com/hva/hva.php?gh_id=4").subscribe(posts => {
-      console.log(posts);
-    })
-  }
 
 }
