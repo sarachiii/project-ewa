95241482da78ef6e8bce7f7e08b7ec84cd4264d5
Jechillo Huang <huangj2@ad.hva.nl>
2021-12-20 16:06:53 +0100

    All the admin pages and services test

---
+++ frontend/src/app/services/admin-guard.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuardGuard implements CanActivate {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+    // the authorization and the authentication of the admin
+    //Calling a user service admin username and admin password mst be correct
+    return false;
+  }
+
+}
