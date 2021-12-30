c5af892e3df47e70706fb4766c2f8e7dcaf94a07
Sarah Chrzanowska-Buth <sarah.chrzanowska-buth@hva.nl>
2021-12-30 17:19:34 +0100

    Functionality of deleting a member from database

--- frontend/src/app/components/teams/view-teams/members/members.component.ts
+++ frontend/src/app/components/teams/view-teams/members/members.component.ts
@@ -2,6 +2,8 @@ import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core'
 import {Role, User} from "../../../../models/user";
 import {TeamService} from "../../../../services/team.service";
 import {first} from "rxjs/operators";
+import {UserService} from "../../../../services/user.service";
+import {NotificationsService} from "angular2-notifications";
 
 @Component({
   selector: 'app-team',
@@ -10,15 +12,15 @@ import {first} from "rxjs/operators";
 })
 export class MembersComponent implements OnInit, OnChanges {
   readonly Role = Role;
+  deleteSuccessful: boolean = false;
   @Input()
   selectedTeamId: number;
-  members: User[];
   public team: User[];
   @Input()
   user: User;
   editTeam: boolean;
 
-  constructor(private teamService: TeamService) {
+  constructor(private teamService: TeamService, private  userService: UserService, private notificationService: NotificationsService) {
     this.team = [];
     this.editTeam = false;
   }
@@ -28,12 +30,29 @@ export class MembersComponent implements OnInit, OnChanges {
 
   ngOnChanges(changes: SimpleChanges) {
     this.teamService.getTeamById(this.selectedTeamId).pipe(first()).subscribe(value => {
-      this.team = value.map(user => Object.assign(new User(), user))
+      this.team = value.map(u => Object.assign(new User(), u))
     });
     this.editTeam = false;
   }
 
-  deleteMember() {
-    console.log("test");
+  deleteMember(id: any) {
+    this.userService.deleteUser(id).subscribe(value => {
+      if(value){
+        let index = this.team.findIndex(member => member.id == id);
+        this.team.splice(index, 1);
+        this.onSucces("User was successfully deleted!");
+      }
+    });
+  }
+
+  onSucces(message){
+    this.deleteSuccessful = true;
+    this.notificationService.success("Success", message, {
+      timeOut: 2000,
+      animate: 'fade',
+    });
+    setTimeout(() => {
+      this.deleteSuccessful = false;
+    }, 3000);
   }
 }
