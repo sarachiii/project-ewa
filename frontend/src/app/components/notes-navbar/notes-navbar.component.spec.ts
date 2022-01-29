ea919239370db936232c61e88caae3e7fb908e8c
Sarah Chrzanowska-Buth <sarah.chrzanowska-buth@hva.nl>
2022-01-29 15:20:01 +0100

    Frontend test ready

--- frontend/src/app/components/notes-navbar/notes-navbar.component.spec.ts
+++ frontend/src/app/components/notes-navbar/notes-navbar.component.spec.ts
@@ -6,11 +6,13 @@ import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {routes} from "../../app-routing.module";
 import {NotesComponent} from "../notes/notes.component";
 import {NotesService} from "../../services/notes.service";
+import {Router} from "@angular/router";
 
 describe('NotesNavbarComponent', () => {
   let component: NotesNavbarComponent;
   let fixture: ComponentFixture<NotesNavbarComponent>;
   let componentHtml: HTMLElement;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -28,24 +30,52 @@ describe('NotesNavbarComponent', () => {
   });
 
   beforeEach(() => {
+    router = TestBed.inject(Router);
     fixture = TestBed.createComponent(NotesNavbarComponent);
     component = fixture.componentInstance;
     componentHtml = fixture.debugElement.nativeElement;
-    fixture.detectChanges();
   });
 
   it('should create the notesNavbar component', () => {
-    let fixture = TestBed.createComponent(NotesNavbarComponent);
-    let component = fixture.debugElement.componentInstance;
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 
-  it('should go to Botany nav-item if clicked on it', () => {
-    let fixture = TestBed.createComponent(NotesNavbarComponent);
-    let component = fixture.debugElement.componentInstance;
-    const navitem: HTMLInputElement = componentHtml.querySelector('#navlink');
-    navitem.textContent = 'Botany';
-    navitem.dispatchEvent(new Event('click'));
-    // TODO add expectation
-  });
+  // it('should create the notesNavbar with the nav-link in uppercase', () => {
+  //
+  //   // Arrange: mock the specialty
+  //   component.specialties = ["Botany"];
+  //
+  //   // Act
+  //   fixture.detectChanges();
+  //
+  //   // Arrange: get UI component
+  //   const navlink: HTMLAnchorElement = componentHtml.querySelector('#navlink');
+  //
+  //   // Assert: check if nav-link shows the correct text
+  //   expect(navlink.innerText).toEqual("BOTANY");
+  //   expect(component).toBeTruthy();
+  // });
+
+  // it('navigates to /botany when the botany nav-item is clicked', () => {
+  //
+  //   // Arrange: mock the specialty
+  //   component.specialties = ["Botany"];
+  //
+  //   // Act
+  //   fixture.detectChanges();
+  //
+  //   // Arrange: get UI component
+  //   const navSpy = spyOn(router, 'navigate');
+  //   const navitem: HTMLAnchorElement = componentHtml.querySelector('#navitem');
+  //
+  //   // Act: click on the nav-item
+  //   navitem.click();
+  //   fixture.detectChanges();
+  //
+  //   //Assert: check if the path is correct
+  //   expect(navSpy).toHaveBeenCalledWith(
+  //     ['botany'],jasmine.anything()
+  //   );
+  // });
 });
--- frontend/src/app/services/notes.service.spec.ts
+++ frontend/src/app/services/notes.service.spec.ts
@@ -1,10 +1,15 @@
 import {TestBed} from '@angular/core/testing';
 
 import {NotesService} from './notes.service';
-import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {Specialty} from "../models/specialty";
+import {Role, User} from "../models/user";
+import {environment} from "../../environments/environment";
+import {Note} from "../models/note";
 
 describe('NotesService', () => {
   let service: NotesService;
+  let httpMock: HttpTestingController
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -12,9 +17,40 @@ describe('NotesService', () => {
       providers: [NotesService]
     });
     service = TestBed.inject(NotesService)
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get all notes available', (done) => {
+
+    // Arrange: mock notes
+    const resourceUrl = environment.apiUrl;
+    let notes: Note[] = []; //create the notes array
+    let mockUser = new User(0, 0, Role.MEMBER, Specialty.GEOLOGY, "Mark", "Smith"); //mock a user
+    let note1 = new Note(0, mockUser, new Date(), "Testnote", "Test text");
+    let note2 = new Note(1, mockUser, new Date(), "Testnote", "Test text");
+    notes.push(note1, note2);  //add notes to the array
+
+    // Act: Call method from the service
+    service.notes$.subscribe((res) => {
+      // Assert: Confirm response is true
+      expect(res).toBeTruthy();
+      expect(res).toBeInstanceOf(Array);
+      if(res.length){
+        expect(res).toEqual(notes);
+      }
+      done();
+    });
+
+    // Assert: URL
+    const req = httpMock.expectOne(`${resourceUrl}/notes/all`);
+    expect(req.request.method).toBe('GET');
+    expect(service).toBeTruthy();
+
+    // Act: Send response
+    req.flush(notes);
   });
 });
